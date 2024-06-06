ddab26d24a4b65ed8ccd854942825c4cf8204f68
Thais Bobato <105284617+thaisbobato@users.noreply.github.com>
2024-06-06 10:14:57 -0300

    ok

---
+++ script.js
@@ -0,0 +1,19 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    { //primeiro objeto da lista de perguntas
+        enunciado: "pergunta1",
+        alternativas: ["alternativa1", "altrnativa2"] 
+
+    },
+    { //segundo objeto da lista de perguntas
+        enunciado: "pergunta2",
+        alternativas: ["alternativa1", "altrnativa2"] 
+
+    }
+
+];
